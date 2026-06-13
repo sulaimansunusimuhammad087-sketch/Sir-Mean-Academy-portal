ef8a91eff64f5bdb0c0b71e8e6ed7403db20149c
sulaimansunusimuhammad087-sketch <sulaimansunusimuhammad087@gmail.com>
2026-06-13 01:30:56 +0100

    Create register.js

---
+++ register.js
@@ -0,0 +1,36 @@
+import { db } from "./firebase-config.js";
+import { ref, push } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-database.js";
+
+const form = document.getElementById("applicationForm");
+
+form.addEventListener("submit", async (e) => {
+e.preventDefault();
+
+```
+const data = {
+    fullName: document.getElementById("fullName").value,
+    phone: document.getElementById("phone").value,
+    email: document.getElementById("email").value,
+    institution: document.getElementById("institution").value,
+    department: document.getElementById("department").value,
+    level: document.getElementById("level").value,
+    course: document.getElementById("course").value,
+    gender: document.getElementById("gender").value,
+    state: document.getElementById("state").value,
+    status: "Pending",
+    dateApplied: new Date().toISOString()
+};
+
+try {
+    await push(ref(db, "applications"), data);
+
+    alert("Application submitted successfully.");
+
+    form.reset();
+
+} catch (error) {
+    alert("Error: " + error.message);
+}
+```
+
+});
