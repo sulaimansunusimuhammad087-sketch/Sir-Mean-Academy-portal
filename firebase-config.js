4bf1df8f85051caf12f7fae88707da719c2edf45
sulaimansunusimuhammad087-sketch <sulaimansunusimuhammad087@gmail.com>
2026-06-18 22:56:11 -0700

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,5 +1,6 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 import { getDatabase } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-database.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 
 const firebaseConfig = {
 apiKey: "AIzaSyDhwNJSe-a5I7OqtrZ2YMBf7Nas_a47MsQ",
@@ -13,3 +14,4 @@ appId: "1:93730400714:web:60450cbecc48aa2a754733"
 
 const app = initializeApp(firebaseConfig);
 export const db = getDatabase(app);
+export const auth = getAuth(app);
